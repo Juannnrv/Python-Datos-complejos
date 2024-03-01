2ef60bad0b0090447c5b4cd7d6575685b655fe2e
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-03-01 06:44:31 -0500

    Tupla info cliente

---
+++ TUPLAS.py
@@ -0,0 +1,5 @@
+años_experiencia = input("Ingrese los años de experiencia laboral que ha obtenido =>")
+cargo = input("Ingresa el último cargo que has ocupado en tu experiencia laboral => ")
+titulo = input("Ingresa el título académico o profesional más relevante para su campo laboral => ")
+experiencia_laboral = tuple((años_experiencia, cargo, titulo))
+print (experiencia_laboral)
